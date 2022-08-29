9f69e7b2b8be4e5e44a8e8b7d7eaf145ceff827e
YOOJS1205 <junsang8921@gmail.com>
2022-08-29 21:17:18 +0900

    Chore: ssr 초기 setting

---
+++ ssr-recipe/src/pages/BluePage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Blue from '../components/Blue/Blue';
+
+export default function BluePage() {
+  return <Blue />;
+}
---
+++ ssr-recipe/src/pages/RedPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Red from '../components/Red/Red';
+
+export default function RedPage() {
+  return <Red />;
+}
