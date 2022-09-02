437a5805990272eb69d787fb2b0dee785270ce98
YOOJS1205 <junsang8921@gmail.com>
2022-09-02 15:59:50 +0900

    Feat: 회원가입, 로그인 기능 구현

---
+++ blog/blog-backend/src/models/user.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new Schema({
+  username: String,
+  hashedPassword: String,
+});
+
+// 인스턴스 메서드 (this는 문서 인스턴스를 가리킨다.)
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function (password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result;
+};
+
+UserSchema.methods.serialize = function () {
+  const data = this.toJSON();
+  delete data.hashedPassword;
+  return data;
+};
+
+// 스태틱 메서드 (this는 model을 가리킨다.)
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username });
+};
+
+const User = mongoose.model('User', UserSchema);
+export default User;
